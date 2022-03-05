8638c50fdcba6bd9ac153062628eb1fafcf32a8d
Hasan Sayfi <hassan.h.sayfi@gmail.com>
2022-03-06 02:18:05 +0300

    Create Bottom App Bar

--- task_management_system/lib/screens/home/homepage.dart
+++ task_management_system/lib/screens/home/homepage.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:task_management_system/models/db_model.dart';
-import 'package:task_management_system/models/todo_model.dart';
-import 'package:task_management_system/widgets/todo_list.dart';
-import 'package:task_management_system/widgets/user_input.dart';
+import 'package:task_management_system/constants/colors.dart';
+import 'package:task_management_system/models/dashboard_cards.dart';
+import 'package:task_management_system/widgets/home_manager.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -10,42 +9,78 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  // We Create insert function here because the 2 widgets can communicate
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _botBarOptions = <Widget>[
+    HomeManager(),
+    Text(
+      'Employees',
+      style: optionStyle,
+    ),
+    Text(
+      'Tasks',
+      style: optionStyle,
+    ),
+  ];
 
-  // Create a DB object to access the DB functions
-  var db = DatabaseConnect();
-
-  // Add function
-  void addItem(Todo todo) async {
-    await db.insertTodo(todo);
-    setState(() {});
-  }
-
-  // Delete function
-  void deleteItem(Todo todo) async {
-    await db.deleteTodo(todo);
-    setState(() {});
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Simple TODO App"),
         foregroundColor: Colors.black,
-        backgroundColor: Colors.white,
+        backgroundColor: Color.fromRGBO(243, 243, 243, 1),
         elevation: 0,
         backwardsCompatibility: false,
-      ),
-      backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          TodoList(
-            insertFunction: addItem,
-            deleteFunction: deleteItem,
+        leading: Container(
+          padding: EdgeInsets.all(10),
+          child: Icon(
+            Icons.menu_sharp,
+            size: 50,
           ),
-          UserInput(insertFunction: addItem),
+        ),
+        actions: [
+          Container(
+            padding: EdgeInsets.only(top: 10),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(100),
+              child: Image.asset(
+                "assets/avatars/img2.png", // TODO: Replace avatar
+              ),
+            ),
+          ),
         ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: kBgColor,
+        elevation: 3,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.groups),
+            label: 'Employees',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.checklist),
+            label: 'Tasks',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+      backgroundColor: kBgColor,
+      body: Center(
+        child: _botBarOptions.elementAt(_selectedIndex),
       ),
     );
   }
