af367cb922381517b3aaec10e1e9e6c401a4447d
Hasan Sayfi <hassan.h.sayfi@gmail.com>
2022-03-06 02:17:24 +0300

    Add backup sqflite model

---
+++ task_management_system/lib/screens/home/homepage_todo.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:task_management_system/models/db_model.dart';
+import 'package:task_management_system/models/todo_model.dart';
+import 'package:task_management_system/widgets/todo_list.dart';
+import 'package:task_management_system/widgets/user_input.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // We Create insert function here because the 2 widgets can communicate
+
+  // Create a DB object to access the DB functions
+  var db = DatabaseConnect();
+
+  // Add function
+  void addItem(Todo todo) async {
+    await db.insertTodo(todo);
+    setState(() {});
+  }
+
+  // Delete function
+  void deleteItem(Todo todo) async {
+    await db.deleteTodo(todo);
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Simple TODO App"),
+        foregroundColor: Colors.black,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        backwardsCompatibility: false,
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          TodoList(
+            insertFunction: addItem,
+            deleteFunction: deleteItem,
+          ),
+          UserInput(insertFunction: addItem),
+        ],
+      ),
+    );
+  }
+}
