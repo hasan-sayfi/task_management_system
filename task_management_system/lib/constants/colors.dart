ff7cc2eb3dbd6852dcc37fec3f52c23bcbc6c3f2
Hasan Sayfi <hassan.h.sayfi@gmail.com>
2022-03-06 02:16:12 +0300

    Set color constants for dashboard and bg

---
+++ task_management_system/lib/constants/colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+const Color kYellowLight = Color(0xFFF6DCBC);
+const Color kYellow = Color(0xFFF4D1A7);
+const Color kYellowDark = Color(0xFFE5A558);
+
+const Color kBlueLight = Color(0xFFB8CCF2);
+const Color kBlue = Color(0xFF9BB6ED);
+const Color kBlueDark = Color(0xFF5081E1);
+
+const Color kGreenLight = Color(0xFFB0F1C1);
+const Color kGreen = Color(0xFF8DECA6);
+const Color kGreenDark = Color(0xFF20BA49);
+
+const Color kRedLight = Color(0xFFF4AEB2);
+const Color kRed = Color(0xFFF09095);
+const Color kRedDark = Color(0xFFEA636A);
+
+const Color kFontBodyColor = Color(0xFF707070);
+const Color kBgColor = Color(0xFFF3F3F3);
