e87c79a452cead286e8c7eab607dc6edad281d17
Hasan Sayfi <hassan.h.sayfi@gmail.com>
2022-03-13 00:02:54 +0300

    Add needed widgets to be built

---
+++ task_management_system/lib/screens/new_department.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewDepartment extends StatelessWidget {
+  const NewDepartment({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ task_management_system/lib/screens/new_employee.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewEmployee extends StatelessWidget {
+  const NewEmployee({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
