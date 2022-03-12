26162d5515a0d3943c1a10fc0758e29dad9b082e
Hasan Sayfi <hassan.h.sayfi@gmail.com>
2022-03-13 00:02:07 +0300

    Add app routes for navigation

--- task_management_system/lib/main.dart
+++ task_management_system/lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:task_management_system/screens/home/homepage.dart';
+import 'package:task_management_system/routes/app_routes.dart';
+import 'package:task_management_system/screens/home/manager/manager_homepage.dart';
+import 'package:task_management_system/screens/login/logout.dart';
+import 'package:task_management_system/screens/new_employee.dart';
+import 'package:task_management_system/screens/new_task.dart';
+import 'package:task_management_system/screens/task_records.dart';
 
 main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,7 +16,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      // home: HomePage(),
+      initialRoute: '/',
+      routes: {
+        AppRoutes.HOME: (context) => HomePage(),
+        AppRoutes.NEW_EMPLOYEE: (context) => NewEmployee(),
+        AppRoutes.NEW_TASK: (context) => NewTask(),
+        AppRoutes.NEW_DEPARTMENT: (context) => NewTask(),
+        AppRoutes.TASK_RECORDS: (context) => TaskRecords(),
+        AppRoutes.LOGOUT: (context) => Logout(),
+      },
     );
   }
 }
---
+++ task_management_system/lib/routes/app_routes.dart
@@ -0,0 +1,11 @@
+class AppRoutes {
+  static const HOME = '/';
+  static const HOME_MANAGER = '/homeManager';
+  static const HOME_EMPLOYEE = '/homeEmployee';
+  static const HOME_ADMIN = '/homeAdmin';
+  static const NEW_TASK = '/newTask';
+  static const NEW_DEPARTMENT = '/newDepartment';
+  static const NEW_EMPLOYEE = '/newEmployee';
+  static const TASK_RECORDS = '/taskRecords';
+  static const LOGOUT = '/logout';
+}
