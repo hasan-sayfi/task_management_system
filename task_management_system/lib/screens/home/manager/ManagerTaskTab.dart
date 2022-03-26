62cddd42e432d39e0b552f858015acc246fc2008
Hasan Sayfi <hassan.h.sayfi@gmail.com>
2022-03-27 02:27:19 +0300

    Create tasks tab

--- task_management_system/lib/screens/home/manager/ManagerTaskTab.dart
+++ task_management_system/lib/screens/home/manager/ManagerTaskTab.dart
@@ -11,44 +11,51 @@ class ManagerTaskTab extends StatefulWidget {
 }
 
 class _ManagerTaskTabState extends State<ManagerTaskTab> {
-  static List<Task> allTasks = [];
+  // static List<Task> allTasks = [];
+  List<Task> allTasks = Task.getTasksWithManager(2);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: allTasks.isEmpty
-          ? Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Text(
-                    'No Tasks added yet!',
-                    style: TextStyle(fontSize: 24, color: kOrange),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                      height: 200,
-                      child: Image.asset(
-                        'assets/background/waiting.png',
-                        fit: BoxFit.cover,
-                      )),
-                ],
+    return Scaffold(
+      backgroundColor: kBgColor,
+      appBar: AppBar(
+        backgroundColor: kBgColor,
+      ),
+      body: Container(
+        child: allTasks.isEmpty
+            ? Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text(
+                      'No Tasks added yet!',
+                      style: TextStyle(fontSize: 24, color: kOrange),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                        height: 200,
+                        child: Image.asset(
+                          'assets/background/waiting.png',
+                          fit: BoxFit.cover,
+                        )),
+                  ],
+                ),
+              )
+            : GridView.builder(
+                itemCount: allTasks.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 1,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  childAspectRatio: 2.3,
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  return Text("TODO");
+                },
               ),
-            )
-          : GridView.builder(
-              itemCount: allTasks.length,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 1,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-                childAspectRatio: 2.3,
-              ),
-              itemBuilder: (BuildContext context, int index) {
-                return Text("TODO");
-              },
-            ),
+      ),
     );
   }
 }
