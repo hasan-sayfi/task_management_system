b7923e70e9c9d4e069420bf158599f80c573550d
Hasan Sayfi <hassan.h.sayfi@gmail.com>
2022-03-26 04:33:19 +0300

    Add theme and assets

--- task_management_system/lib/constants/colors.dart
+++ task_management_system/lib/constants/colors.dart
@@ -18,3 +18,7 @@ const Color kRedDark = Color(0xFFEA636A);
 
 const Color kFontBodyColor = Color(0xFF707070);
 const Color kBgColor = Color(0xFFF3F3F3);
+
+const Color kOrangeLight = Color(0xFFFFB233);
+const Color kOrange = Color(0xFFFF8F00);
+const Color kOrangeDark = Color(0xFFD45D00);
