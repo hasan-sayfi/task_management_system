213c086c3489e47c14fd93f0f2e6d6889c677077
Hasan Sayfi <hassan.h.sayfi@gmail.com>
2022-03-26 04:37:08 +0300

    Add new widget for organizing employee tab

---
+++ task_management_system/lib/widgets/manager_employee_list.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:task_management_system/constants/colors.dart';
+import 'package:task_management_system/models/employee.dart';
+import 'package:task_management_system/widgets/build_employee_card.dart';
+
+class ManagerEmployeeList extends StatelessWidget {
+  final List<Employee> employees;
+
+  ManagerEmployeeList(this.employees);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(15),
+      height: 520,
+      child: employees.isEmpty
+          ? Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text(
+                    'No Employees added yet!',
+                    style: TextStyle(fontSize: 24, color: kOrange),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                      height: 200,
+                      child: Image.asset(
+                        'assets/background/waiting.png',
+                        fit: BoxFit.cover,
+                      )),
+                ],
+              ),
+            )
+          : GridView.builder(
+              itemCount: employees.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 1,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                childAspectRatio: 2.3,
+              ),
+              itemBuilder: (BuildContext context, int index) {
+                return BuildEmployeeCard(
+                  context: context,
+                  employee: employees[index],
+                );
+              },
+            ),
+    );
+  }
+}
