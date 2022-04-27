7081c17f4fbbf7657ce36b8e1a9960a6e052b29d
Hasan Sayfi <hassan.h.sayfi@gmail.com>
2022-04-27 21:30:21 +0300

    Add CRUD methods

--- task_management_system/lib/models/database_connection.dart
+++ task_management_system/lib/models/database_connection.dart
@@ -1,108 +1,270 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart'; // sqflite for database
-import 'package:path/path.dart';
+import 'package:task_management_system/models/department.dart';
+import 'package:task_management_system/models/employee.dart';
 import 'package:task_management_system/models/models_test/todo_model.dart';
+import 'package:task_management_system/models/role.dart';
+import 'package:task_management_system/models/task.dart';
+import 'package:task_management_system/script/table_fields.dart';
+import 'package:task_management_system/script/table_names.dart';
+import 'package:task_management_system/script/table_queries.dart';
 
 class DatabaseConnect {
+  static DatabaseConnect? _databaseConnect; //Singleton DatabaseHelper
+  static Database? _database; //Singleton Database
+  static const String DB_NAME = 'task_management_system1';
+
   // Tables Queries:
-  final employeeSQL = '''
-      CREATE TABLE "Employee" (
-        "empID"	INTEGER NOT NULL,
-        "roleID"	INTEGER NOT NULL,
-        "deptID"	INTEGER NOT NULL,
-        "empName"	TEXT NOT NULL,
-        "empEmail"	TEXT NOT NULL,
-        "empMobile"	TEXT,
-        "empAddress"	TEXT,
-        "empAvatar"	TEXT,
-        FOREIGN KEY("deptID") REFERENCES "Department"("deptID"),
-        FOREIGN KEY("roleID") REFERENCES "Role"("roleID"),
-        PRIMARY KEY("empID" AUTOINCREMENT)
-      );
-      ''';
-  final departmentSQL = '''
-      CREATE TABLE "Department" (
-        "deptID"	INTEGER NOT NULL,
-        "deptName"	TEXT NOT NULL,
-        PRIMARY KEY("deptID" AUTOINCREMENT)
-      );
-      ''';
-  final taskSQL = '''
-      CREATE TABLE "Task" (
-        "taskID"	INTEGER,
-        "taskName"	TEXT NOT NULL,
-        "taskDesc"	TEXT NOT NULL,
-        "taskComment"	TEXT,
-        "taskStartDate"	TEXT NOT NULL,
-        "taskEndDate"	TEXT NOT NULL,
-        "taskProgress"	INTEGER NOT NULL DEFAULT 0,
-        "taskStatus"	INTEGER NOT NULL DEFAULT 0,
-        PRIMARY KEY("taskID" AUTOINCREMENT)
-      );
-      ''';
-  final roleSQL = '''
-      CREATE TABLE "Role" (
-        "roleID"	INTEGER NOT NULL,
-        "roleName"	TEXT NOT NULL,
-        PRIMARY KEY("roleID" AUTOINCREMENT)
-      );
-      ''';
-  final employeeTaskRecordSQL = '''
-      CREATE TABLE "EmployeeTaskRecord" (
-        "recID"	INTEGER NOT NULL,
-        "empID"	INTEGER NOT NULL,
-        "taskID"	INTEGER NOT NULL,
-        "finishedDate"	TEXT NOT NULL,
-        FOREIGN KEY("empID") REFERENCES "Employee"("empID"),
-        FOREIGN KEY("taskID") REFERENCES "Task"("taskID"),
-        PRIMARY KEY("recID" AUTOINCREMENT)
-      );
-      ''';
-  final todoSQL = '''
-      CREATE TABLE "todo"(
-        "id" INTEGER PRIMARY KEY AUTOINCREMENT, 
-        "title" TEXT, 
-        "creationDate" TEXT, 
-        "isChecked" INTEGER,
-        PRIMARY KEY("recID" AUTOINCREMENT)
-      )
-      ''';
+  final departmentSQL = TableQueries.departmentSQL;
+  final roleSQL = TableQueries.roleSQL;
+  final employeeSQL = TableQueries.employeeSQL;
+  final taskSQL = TableQueries.taskSQL;
+  final employeeTaskRecordSQL = TableQueries.employeeTaskRecordSQL;
 
-  Database? _database;
+  DatabaseConnect._createInstance();
 
-  // Create a getter and open a connection to database
+  factory DatabaseConnect() {
+    if (_databaseConnect == null) {
+      _databaseConnect = DatabaseConnect._createInstance();
+    }
+    return _databaseConnect!;
+  }
+
   Future<Database?> get database async {
-    // This is the location for the DB in device. ex - data/data/...
-    final dbPath = await getDatabasesPath();
-    // This is the name of our DB
-    const dbName = 'task_management_system.db';
-    // This joins the db_path and db_name and creates a full path for DB
-    // ex - data/data/todo.db
-    final path = join(dbPath, dbName);
-
-    // Open the connection
-    _database = await openDatabase(path, version: 1, onCreate: _createDB);
-
+    if (_database == null) {
+      _database = await initializeDatabase();
+    }
     return _database;
   }
 
-  // Create the _createDB function seperately
-  // This creates Tables in the DB
-  Future<void> _createDB(Database db, int version) async {
-    // Ensure that the columns in the DB match the todo_model fields
-    await db.execute(todoSQL);
+  Future<Database> initializeDatabase() async {
+    // This is the location for the DB in device. ex - data/data/...
+    Directory directory = await getApplicationDocumentsDirectory();
+
+    // This joins the db_path and db_name and creates a full path for DB
+    // ex - data/data/todo.db
+    String path = directory.path + DB_NAME;
+
+    // Open/Create the database at the given path
+    var taskDatabase =
+        await openDatabase(path, version: 1, onCreate: _createDB);
+
+    return taskDatabase;
   }
 
-  // Insert function
-  Future<void> insertTodo(Todo todo) async {
-    // Get connection to DB
-    final db = await database;
-    // Insert the todo
-    await db!.insert(
-      'todo', // Table name
-      todo.toMap(), // Function created in todo_model
+  /*
+  Create the _createDB function seperately
+  This creates Tables in the DB 
+  */
+  Future<void> _createDB(Database db, int version) async {
+    // Ensure that the columns in the DB match the todo_model fields
+    await db.execute(departmentSQL);
+    await db.execute(roleSQL);
+    await db.execute(employeeSQL);
+    await db.execute(taskSQL);
+    await db.execute(employeeTaskRecordSQL);
+  }
+
+  /* START SELECT SECTION */
+
+  //Fetch Operation: Get all Employee objects from database
+  Future<List<Map<String, dynamic>>> getEmployeeMapList(int? deptID) async {
+    Database? db = await this.database;
+    //var result = db.rawQuery('SELECT * FROM $taskTable order by $colDate, $colTime ASC');
+    var result;
+    if (deptID == null) {
+      result =
+          db!.query(TableNames.empTableName, orderBy: '${TableFields.empID}');
+    } else {
+      result = db!.query(TableNames.empTableName,
+          where: '${TableFields.deptID} = ? and ${TableFields.roleID} = ?',
+          whereArgs: [deptID, 3],
+          orderBy: '${TableFields.empName}');
+    }
+    return result;
+  }
+
+  //Fetch Operation: Get all Task objects from database
+  Future<List<Map<String, dynamic>>> getTaskMapList(
+    int deptID,
+  ) async {
+    Database? db = await this.database;
+    var result = db!.rawQuery(
+        'SELECT * FROM ${TableNames.taskTableName} t WHERE t.empID IN (SELECT e.empID FROM Employee e WHERE e.deptID = $deptID)');
+    /* var result = db!.query(TableNames.taskTableName,
+        where: '${TableFields.empID} = ?',
+        whereArgs: [empID],
+        orderBy: '${TableFields.taskStartDate}'); */
+    return result;
+  }
+
+  //Fetch Operation: Get all Department objects from database
+  Future<List<Map<String, dynamic>>> getDepartmentMapList(int? deptID) async {
+    Database? db = await this.database;
+    //var result = db.rawQuery('SELECT * FROM $taskTable order by $colDate, $colTime ASC');
+    var result;
+    if (deptID == null)
+      result = db!.query(TableNames.deptTableName);
+    else
+      result = db!.query(TableNames.deptTableName,
+          where: '${TableFields.deptID} = ?', whereArgs: [deptID]);
+
+    return result;
+  }
+
+  //Fetch Operation: Get all Role objects from database
+  Future<List<Map<String, dynamic>>> getRoleMapList() async {
+    Database? db = await this.database;
+    //var result = db.rawQuery('SELECT * FROM $taskTable order by $colDate, $colTime ASC');
+    var result = db!.query(TableNames.roleTableName);
+    return result;
+  }
+  /* END SELECT SECTION */
+
+  /* START INSERT SECTION */
+
+  //Insert Operation: Insert a Task object to database
+  Future<int> insertTask(Task task) async {
+    Database? db = await this.database;
+    var result = await db!.insert(TableNames.taskTableName, task.toMap());
+    return result;
+  }
+
+  //Insert Operation: Insert a Role object to database
+  Future<int> insertRole(Role role) async {
+    Database? db = await this.database;
+    var result = await db!.insert(TableNames.roleTableName, role.toMap());
+    return result;
+  }
+
+  //Insert Operation: Insert a Department object to database
+  Future<int> insertDepartment(Department department) async {
+    Database? db = await this.database;
+    var result = await db!.insert(TableNames.deptTableName, department.toMap());
+    return result;
+  }
+
+  //Insert Operation: Insert an Employee object to database
+  Future<int> insertEmployee(Employee employee) async {
+    Database? db = await this.database;
+    var result = await db!.insert(TableNames.empTableName, employee.toMap());
+    return result;
+  }
+  /* END INSERT SECTION */
+
+  /* START UPDATE SECTION */
+  //Update Operation: Update a Role object to database
+  Future<int> updateRole(Role role) async {
+    Database? db = await this.database;
+    var result = await db!.update(
+      TableNames.roleTableName, role.toMap(),
+      where: '${TableFields.roleID} = ?',
+      whereArgs: [role.roleID],
       conflictAlgorithm:
           ConflictAlgorithm.replace, // This helps to replace the dublicates
     );
+
+    return result;
+  }
+
+  //Update Operation: Update a Department object to database
+  Future<int> updateDepartment(Department department) async {
+    Database? db = await this.database;
+    var result = await db!.update(
+      TableNames.deptTableName, department.toMap(),
+      where: '${TableFields.deptID} = ?', whereArgs: [department.deptID],
+      conflictAlgorithm:
+          ConflictAlgorithm.replace, // This helps to replace the dublicates
+    );
+
+    return result;
+  }
+
+  //Update Operation: Update a Employee object to database
+  Future<int> updateEmployee(Employee employee) async {
+    Database? db = await this.database;
+    var result = await db!.update(
+      TableNames.empTableName, employee.toMap(),
+      where: '${TableFields.empID} = ?', whereArgs: [employee.empID],
+      conflictAlgorithm:
+          ConflictAlgorithm.replace, // This helps to replace the dublicates
+    );
+
+    return result;
+  }
+  /* END UPDATE SECTION */
+
+  /* START DELETE SECTION */
+  //Delete Operation: Delete a Role object in database
+  Future<int> deleteRole(Role role) async {
+    Database? db = await this.database;
+    // var result = await db!.delete(TableNames.roleTableName, role.toMap(),
+    //     where: '${TableFields.roleID} = ?', whereArgs: [role.roleID]);
+    var result = await db!.delete(TableNames.roleTableName,
+        where: '${TableFields.roleID} = ?', whereArgs: [role.roleID]);
+
+    int count = await db.rawUpdate(
+        'UPDATE sqlite_sequence SET seq = (SELECT MAX(${TableFields.roleID}) FROM ${TableNames.roleTableName}) WHERE name="${TableNames.roleTableName}"');
+
+    print('deleteRole: $count');
+
+    return result;
+  }
+
+  //Delete Operation: Delete all Roles object in database
+  Future<int> deleteAllRoles() async {
+    Database? db = await this.database;
+    var result = await db!.rawDelete('DELETE FROM ${TableNames.roleTableName}');
+
+    int count = await db.rawUpdate(
+        'UPDATE sqlite_sequence SET seq = (SELECT MAX(${TableFields.roleID}) FROM ${TableNames.roleTableName}) WHERE name="${TableNames.roleTableName}"');
+
+    print('deleteAllRole: $count');
+
+    return result;
+  }
+
+  //Delete Operation: Delete a Department object in database
+  Future<int> deleteDepartment(Department department) async {
+    Database? db = await this.database;
+    var result = await db!.delete(TableNames.deptTableName,
+        where: '${TableFields.deptID} = ?', whereArgs: [department.deptID]);
+
+    int count = await db.rawUpdate(
+        'UPDATE sqlite_sequence SET seq = (SELECT MAX(${TableFields.deptID}) FROM ${TableNames.deptTableName}) WHERE name="${TableNames.deptTableName}"');
+
+    print('deleteRole: $count');
+
+    return result;
+  }
+
+  //Delete Operation: Delete all Departments object in database
+  Future<int> deleteAllDepartments() async {
+    Database? db = await this.database;
+    var result = await db!.rawDelete('DELETE FROM ${TableNames.deptTableName}');
+
+    int count = await db.rawUpdate(
+        'UPDATE sqlite_sequence SET seq = (SELECT MAX(${TableFields.deptID}) FROM ${TableNames.deptTableName}) WHERE name="${TableNames.deptTableName}"');
+
+    print('deleteAllDepartments: $count');
+
+    return result;
+  }
+
+  /* END DELETE SECTION */
+
+  Future<List<Role>> getRoleList() async {
+    var roleMapList = await getRoleMapList(); //Get Map List from database
+    int count = roleMapList.length;
+
+    List<Role> roleList = <Role>[];
+    //For loop to create Task List from a Map List
+    for (int i = 0; i < count; i++) {
+      roleList.add(Role.fromMapObject(roleMapList[i]));
+    }
+    return roleList;
   }
 
   // Delete function
